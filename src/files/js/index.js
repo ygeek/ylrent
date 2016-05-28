e92ff3e07a0a2b6a42487efcea76c416f49b0317
hujianchuang <hujianchuang@192.168.199.110>
2016-05-28 20:26:22 +0800

    PC端html加程序

---
+++ src/files/js/index.js
@@ -0,0 +1,43 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+jQuery(".fullSlide").hover(function() {
+    jQuery(this).find(".prev,.next").stop(true, true).fadeTo("show", 0.5)
+},
+function() {
+    jQuery(this).find(".prev,.next").fadeOut()
+});
+jQuery(".fullSlide").slide({
+    titCell: ".hd ul",
+    mainCell: ".bd ul",
+    effect: "fold",
+    autoPlay: true,
+    autoPage: true,
+    trigger: "click",
+    startFun: function(i) {
+        var curLi = jQuery(".fullSlide .bd li").eq(i);
+        if ( !! curLi.attr("_src")) {
+            curLi.css("background-image", curLi.attr("_src")).removeAttr("_src")
+        }
+    }
+});
+
+Qfast.add('widgets', { path: "files/js/terminator2.2.min.js", type: "js", requires: ['fx'] });  
+Qfast(false, 'widgets', function () {
+        K.tabs({
+                id: 'fsD1',   //焦点图包裹id  
+                conId: "D1pic1",  //** 大图域包裹id  
+                tabId:"D1fBt",  
+                tabTn:"a",
+                conCn: '.fcon', //** 大图域配置class       
+                auto: 1,   //自动播放 1或0
+                effect: 'fade',   //效果配置
+                eType: 'click', //** 鼠标事件
+                pageBt:true,//是否有按钮切换页码
+                bns: ['.prev', '.next'],//** 前后按钮配置class                          
+                interval: 3000  //** 停顿时间  
+        }) 
+})  
+
