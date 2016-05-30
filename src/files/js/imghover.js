be49f44110a6c4648ef726a7d473bb53eecd7bdc
nightfade <nightfade.blue@gmail.com>
2016-05-31 04:36:42 +0800

    ADD: slides

--- src/files/js/imghover.js
+++ src/files/js/imghover.js
@@ -3,6 +3,10 @@
  */
 
 $('.imghover').hover(
-  function() { $( this ).fadeTo( 'fast', '0.7'); },
-  function() { $( this ).fadeTo( 'fast', '1'); }
+  function() {
+    $(this).fadeTo( 'fast', '0.7');
+  },
+  function() {
+    $(this).fadeTo( 'fast', '1');
+  }
 );
