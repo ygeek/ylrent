017c3281aea1996bbcacd72b647a7e7bc871ee3d
nightfade <nightfade.blue@gmail.com>
2016-06-26 09:36:53 +0800

    ADD: image function for cms editor

--- src/files/lib/ckeditor/build-config.js
+++ src/files/lib/ckeditor/build-config.js
@@ -13,10 +13,10 @@
  * (1) http://ckeditor.com/builder
  *     Visit online builder to build CKEditor from scratch.
  *
- * (2) http://ckeditor.com/builder/64d7fd40fd901c8f0da0938f8070dfb5
+ * (2) http://ckeditor.com/builder/c30722e5aa70351515c2cee2397f41c1
  *     Visit online builder to build CKEditor, starting with the same setup as before.
  *
- * (3) http://ckeditor.com/builder/download/64d7fd40fd901c8f0da0938f8070dfb5
+ * (3) http://ckeditor.com/builder/download/c30722e5aa70351515c2cee2397f41c1
  *     Straight download link to the latest version of CKEditor (Optimized) with the same setup as before.
  *
  * NOTE:
@@ -25,8 +25,8 @@
  */
 
 var CKBUILDER_CONFIG = {
-	skin: 'bootstrapck',
-	preset: 'basic',
+	skin: 'moono',
+	preset: 'standard',
 	ignore: [
 		'.bender',
 		'bender.js',
@@ -50,17 +50,37 @@ var CKBUILDER_CONFIG = {
 		'tests'
 	],
 	plugins : {
-		'about' : 1,
 		'basicstyles' : 1,
+		'blockquote' : 1,
 		'clipboard' : 1,
+		'contextmenu' : 1,
+		'elementspath' : 1,
 		'enterkey' : 1,
 		'entities' : 1,
 		'floatingspace' : 1,
+		'format' : 1,
+		'horizontalrule' : 1,
+		'htmlwriter' : 1,
+		'image' : 1,
 		'indentlist' : 1,
 		'link' : 1,
 		'list' : 1,
+		'magicline' : 1,
+		'maximize' : 1,
+		'pastefromword' : 1,
+		'pastetext' : 1,
+		'removeformat' : 1,
+		'resize' : 1,
+		'showborders' : 1,
+		'sourcearea' : 1,
+		'specialchar' : 1,
+		'stylescombo' : 1,
+		'tab' : 1,
+		'table' : 1,
+		'tabletools' : 1,
 		'toolbar' : 1,
 		'undo' : 1,
+		'wsc' : 1,
 		'wysiwygarea' : 1
 	},
 	languages : {
